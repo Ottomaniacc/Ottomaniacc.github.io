c29587ebb260e8ae3315ff3bf6b1ff240f3a4022
Ottomaniacc <302899406@student.rocmondriaan.nl>
2023-04-19 10:19:47 +0200

    projects toegevoegd in profile Features

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let profileFeatures = {
     dateOfBirth:"08-02-2004",
     profilePicture: "https://avatars.githubusercontent.com/u/118803534?s=400&u=aa82507377bb4003806b9e82df4a0e330b2a9adf&v=4",
     bioGraphy: "Hoi, Ik ben Zinedine Zidane Mese 19 jaar oud en een student bij ROC mondriaan als Software developer.",
+    projects:"Hoger/Lager, TicTacToe, WebCatalogus"
     educations: "Niveau 2 supportdesk medewerker, Niveau 4 Software Developer",
     skills: "kickboxing, Gunsmithing, Coding, Chess",
     bannerColor:"grey",
