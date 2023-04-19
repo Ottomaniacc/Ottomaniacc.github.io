c0929eb2148401f8936235b014bd08c302af691a
Ottomaniacc <302899406@student.rocmondriaan.nl>
2023-04-19 10:14:24 +0200

    main.js profile Features toegevoegd als object

---
+++ main.js
@@ -0,0 +1,11 @@
+let profileFeatures = {
+    emailAddress:"Zinedine321@hotmail.com",
+    userName:"Ottomaniacc",
+    dateOfBirth:"08-02-2004",
+    profilePicture: "https://avatars.githubusercontent.com/u/118803534?s=400&u=aa82507377bb4003806b9e82df4a0e330b2a9adf&v=4",
+    bioGraphy: "Hoi, Ik ben Zinedine Zidane Mese 19 jaar oud en een student bij ROC mondriaan als Software developer.",
+    educations: "Niveau 2 supportdesk medewerker, Niveau 4 Software Developer",
+    skills: "kickboxing, Gunsmithing, Coding, Chess",
+    bannerColor:"grey",
+    socialMedias:"Steam, VKontakt, Telegram"
+}
